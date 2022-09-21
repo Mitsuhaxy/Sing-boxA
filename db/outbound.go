7a51009f3f2e1321fb03965b1eca1314665b956b
悠 <i@mengxy.me>
2022-09-22 01:48:43 +0800

    update

--- db/outbound.go
+++ db/outbound.go
@@ -8,6 +8,14 @@ import (
 )
 
 func Add_Outbound_Shadowsocks(outbound models.Outbound_Shadowsocks) (isSuccess bool) {
+	rows, _ := DB().Query("SELECT name FROM outbound")
+	for rows.Next() {
+		var name string
+		rows.Scan(&name)
+		if outbound.Tag == name {
+			return false
+		}
+	}
 	outboundJson, _ := json.Marshal(outbound)
 	name := outbound.Tag
 	db, err := DB().Prepare("INSERT INTO outbound(name, data) VALUES(?, ?)")
@@ -16,91 +24,120 @@ func Add_Outbound_Shadowsocks(outbound models.Outbound_Shadowsocks) (isSuccess b
 }
 
 func Add_Outbound_VLESS(outbound models.Outbound_VLESS) (isSuccess bool) {
+	rows, _ := DB().Query("SELECT name FROM outbound")
+	for rows.Next() {
+		var name string
+		rows.Scan(&name)
+		if outbound.Tag == name {
+			return false
+		}
+	}
 	outboundJson, _ := json.Marshal(outbound)
 	fmt.Println(string(outboundJson))
-	name := outbound.Tag
 	db, err := DB().Prepare("INSERT INTO outbound(name, data) VALUES(?, ?)")
-	db.Exec(name, string(outboundJson))
+	db.Exec(outbound.Tag, string(outboundJson))
 	return err == nil
 }
 
 func Add_Outbound_VMess(outbound models.Outbound_VMess) (isSuccess bool) {
+	rows, _ := DB().Query("SELECT name FROM outbound")
+	for rows.Next() {
+		var name string
+		rows.Scan(&name)
+		if outbound.Tag == name {
+			return false
+		}
+	}
 	outboundJson, _ := json.Marshal(outbound)
-	name := outbound.Tag
 	db, err := DB().Prepare("INSERT INTO outbound(name, data) VALUES(?, ?)")
-	db.Exec(name, string(outboundJson))
+	db.Exec(outbound.Tag, string(outboundJson))
 	return err == nil
 }
 
 func Add_Outbound_Trojan(outbound models.Outbound_Trojan) (isSuccess bool) {
+	rows, _ := DB().Query("SELECT name FROM outbound")
+	for rows.Next() {
+		var name string
+		rows.Scan(&name)
+		if outbound.Tag == name {
+			return false
+		}
+	}
 	outboundJson, _ := json.Marshal(outbound)
-	name := outbound.Tag
 	db, err := DB().Prepare("INSERT INTO outbound(name, data) VALUES(?, ?)")
-	db.Exec(name, string(outboundJson))
+	db.Exec(outbound.Tag, string(outboundJson))
 	return err == nil
 }
 
 func Add_Outbound_WireGuard(outbound models.Outbound_WireGuard) (isSuccess bool) {
+	rows, _ := DB().Query("SELECT name FROM outbound")
+	for rows.Next() {
+		var name string
+		rows.Scan(&name)
+		if outbound.Tag == name {
+			return false
+		}
+	}
 	outboundJson, _ := json.Marshal(outbound)
-	name := outbound.Tag
 	db, err := DB().Prepare("INSERT INTO outbound(name, data) VALUES(?, ?)")
-	db.Exec(name, string(outboundJson))
+	db.Exec(outbound.Tag, string(outboundJson))
 	return err == nil
 }
 
 func Add_Outbound_Hysteria(outbound models.Outbound_Hysteria) (isSuccess bool) {
+	rows, _ := DB().Query("SELECT name FROM outbound")
+	for rows.Next() {
+		var name string
+		rows.Scan(&name)
+		if outbound.Tag == name {
+			return false
+		}
+	}
 	outboundJson, _ := json.Marshal(outbound)
-	name := outbound.Tag
 	db, err := DB().Prepare("INSERT INTO outbound(name, data) VALUES(?, ?)")
-	db.Exec(name, string(outboundJson))
+	db.Exec(outbound.Tag, string(outboundJson))
 	return err == nil
 }
 
 func Mod_Outbound_Shadowsocks(outbound models.Outbound_Shadowsocks) (isSuccess bool) {
 	outboundJson, _ := json.Marshal(outbound)
-	name := outbound.Tag
 	db, err := DB().Prepare("UPDATE outbound SET data = ? WHERE name = ?")
-	db.Exec(string(outboundJson), name)
+	db.Exec(string(outboundJson), outbound.Tag)
 	return err == nil
 }
 
 func Mod_Outbound_VLESS(outbound models.Outbound_VLESS) (isSuccess bool) {
 	outboundJson, _ := json.Marshal(outbound)
-	name := outbound.Tag
 	db, err := DB().Prepare("UPDATE outbound SET data = ? WHERE name = ?")
-	db.Exec(string(outboundJson), name)
+	db.Exec(string(outboundJson), outbound.Tag)
 	return err == nil
 }
 
 func Mod_Outbound_VMess(outbound models.Outbound_VMess) (isSuccess bool) {
 	outboundJson, _ := json.Marshal(outbound)
-	name := outbound.Tag
 	db, err := DB().Prepare("UPDATE outbound SET data = ? WHERE name = ?")
-	db.Exec(string(outboundJson), name)
+	db.Exec(string(outboundJson), outbound.Tag)
 	return err == nil
 }
 
 func Mod_Outbound_Trojan(outbound models.Outbound_Trojan) (isSuccess bool) {
 	outboundJson, _ := json.Marshal(outbound)
-	name := outbound.Tag
 	db, err := DB().Prepare("UPDATE outbound SET data = ? WHERE name = ?")
-	db.Exec(string(outboundJson), name)
+	db.Exec(string(outboundJson), outbound.Tag)
 	return err == nil
 }
 
 func Mod_Outbound_WireGuard(outbound models.Outbound_WireGuard) (isSuccess bool) {
 	outboundJson, _ := json.Marshal(outbound)
-	name := outbound.Tag
 	db, err := DB().Prepare("UPDATE outbound SET data = ? WHERE name = ?")
-	db.Exec(string(outboundJson), name)
+	db.Exec(string(outboundJson), outbound.Tag)
 	return err == nil
 }
 
 func Mod_Outbound_Hysteria(outbound models.Outbound_Hysteria) (isSuccess bool) {
 	outboundJson, _ := json.Marshal(outbound)
-	name := outbound.Tag
 	db, err := DB().Prepare("UPDATE outbound SET data = ? WHERE name = ?")
-	db.Exec(string(outboundJson), name)
+	db.Exec(string(outboundJson), outbound.Tag)
 	return err == nil
 }
 

455c539abc9aac29b32b7e9c5c9be6ca7446082c
悠 <i@mengxy.me>
2022-09-22 22:18:05 +0800

    update

--- db/status.go
+++ db/status.go
@@ -1,9 +1,12 @@
 package db
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 func Instance(command string) (isSuccess bool) {
 	db, err := DB().Prepare("UPDATE status SET value = ? WHERE key = 'status'")
 =======
+=======
+>>>>>>> parent of 2734e56 (update)
 func Status(command string) (isSuccess bool) {
 	db, err := DB().Prepare("UPDATE status SET data = ? WHERE name = 'status'")
 >>>>>>> parent of 2734e56 (update)

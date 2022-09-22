070b5445b2ee8a8371748083f25ebc34ab37f6ac
悠 <i@mengxy.me>
2022-09-22 23:55:37 +0800

    update

--- db/route.go
+++ db/route.go
@@ -7,7 +7,7 @@ import (
 
 func Route(route models.Route) (isSuccess bool) {
 	setLogJson, _ := json.Marshal(route)
-	db, err := DB().Prepare("UPDATE log SET value = ? WHERE key = 'log'")
+	db, err := DB().Prepare("UPDATE route SET data = ? WHERE name = 'route'")
 	db.Exec(setLogJson)
 	return err == nil
 }

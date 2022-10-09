d384b16704878c7e19f47ee1cde2865c9f65118b
悠 <i@mengxy.me>
2022-10-09 15:47:26 +0800

    Update generator.go

--- models/generator.go
+++ models/generator.go
@@ -22,36 +22,36 @@ type ConfigOutbounds struct {
 	Type                  string    `json:"type"`
 	Server                string    `json:"server"`
 	Server_port           int       `json:"server_port"`
-	Method                string    `json:"method,omitempty"`
-	Plugin                string    `json:"plugin,omitempty"`
-	Plugin_opts           string    `json:"plugin_opts,omitempty"`
-	Obfs                  string    `json:"obfs,omitempty"`
-	Obfs_param            string    `json:"obfs_param,omitempty"`
-	Protocol              string    `json:"protocol,omitempty"`
-	Protocol_param        string    `json:"protocol_param,omitempty"`
-	Password              string    `json:"password,omitempty"`
-	Udp_over_tcp          bool      `json:"udp_over_tcp,omitempty"`
-	TLS                   TLS       `json:"tls,omitempty"`
-	Packet_encoding       string    `json:"packet_encoding,omitempty"`
-	Transport             Transport `json:"transport,omitempty"`
-	UUID                  string    `json:"uuid,omitempty"`
-	Security              string    `json:"security,omitempty"`
-	System_interface      bool      `json:"system_interface,omitempty"`
-	Interface_name        string    `json:"interface_name,omitempty"`
-	Local_address         []string  `json:"local_address,omitempty"`
-	Private_key           string    `json:"private_key,omitempty"`
-	Peer_public_key       string    `json:"peer_public_key,omitempty"`
-	Pre_shared_key        string    `json:"pre_shared_key,omitempty"`
-	Mtu                   int       `json:"mtu,omitempty"`
-	Up                    string    `json:"up,omitempty"`
-	Up_mbps               int       `json:"up_mbps,omitempty"`
-	Down                  string    `json:"down,omitempty"`
-	Down_mbps             int       `json:"down_mbps,omitempty"`
-	Auth                  string    `json:"auth,omitempty"`
-	Auth_str              string    `json:"auth_str,omitempty"`
-	Recv_window_conn      int       `json:"recv_window_conn,omitempty"`
-	Recv_window           int       `json:"recv_window,omitempty"`
-	Disable_mtu_discovery bool      `json:"disable_mtu_discovery,omitempty"`
+	Method                string    `json:"method"`
+	Plugin                string    `json:"plugin"`
+	Plugin_opts           string    `json:"plugin_opts"`
+	Obfs                  string    `json:"obfs"`
+	Obfs_param            string    `json:"obfs_param"`
+	Protocol              string    `json:"protocol"`
+	Protocol_param        string    `json:"protocol_param"`
+	Password              string    `json:"password"`
+	Udp_over_tcp          bool      `json:"udp_over_tcp"`
+	TLS                   TLS       `json:"tls"`
+	Packet_encoding       string    `json:"packet_encoding"`
+	Transport             Transport `json:"transport"`
+	UUID                  string    `json:"uuid"`
+	Security              string    `json:"security"`
+	System_interface      bool      `json:"system_interface"`
+	Interface_name        string    `json:"interface_name"`
+	Local_address         []string  `json:"local_address"`
+	Private_key           string    `json:"private_key"`
+	Peer_public_key       string    `json:"peer_public_key"`
+	Pre_shared_key        string    `json:"pre_shared_key"`
+	Mtu                   int       `json:"mtu"`
+	Up                    string    `json:"up"`
+	Up_mbps               int       `json:"up_mbps"`
+	Down                  string    `json:"down"`
+	Down_mbps             int       `json:"down_mbps"`
+	Auth                  string    `json:"auth"`
+	Auth_str              string    `json:"auth_str"`
+	Recv_window_conn      int       `json:"recv_window_conn"`
+	Recv_window           int       `json:"recv_window"`
+	Disable_mtu_discovery bool      `json:"disable_mtu_discovery"`
 }
 
 type ConfigRules struct {

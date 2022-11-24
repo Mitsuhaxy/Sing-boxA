303a3fc3a336dc30346de12127acaefb24c43fd0
悠 <i@mengxy.me>
2022-11-24 15:54:32 +0800

    update

--- main.go
+++ main.go
@@ -9,13 +9,13 @@ import (
 const SBA_VERSION string = "b1.0"
 
 func main() {
-	
-		switch os.Args[1] {
-		case "run":
-			api.RunApi()
-		case "version":
-			fmt.Println(SBA_VERSION)
-		}
-	
-	
+	switch os.Args[1] {
+	case "run":{
+		api.RunApi()
+	}	
+	case "version":
+		fmt.Println(SBA_VERSION)
+	case "help":
+		fmt.Println("YOU DONT NEED HELP!")
+	}
 }

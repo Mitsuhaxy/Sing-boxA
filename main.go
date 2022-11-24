5f8cb9fb6b410de02097839190ef54478c545bbd
悠 <i@mengxy.me>
2022-11-24 15:45:36 +0800

    fix

--- main.go
+++ main.go
@@ -9,13 +9,13 @@ import (
 const SBA_VERSION string = "b1.0"
 
 func main() {
-	for _, args := range os.Args {
-		switch args[1] {
+	
+		switch os.Args[1] {
 		case "run":
 			api.RunApi()
 		case "version":
 			fmt.Println(SBA_VERSION)
 		}
-	}
+	
 	
 }
